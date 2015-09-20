db3126fa5076296fb72b6bed79a3bbe518b68928
Harrison Gregg <animasterh@gmail.com>
2015-09-20 00:06:31 -0400

    Updated UI

---
+++ frontend/migrations/0001_initial.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+import django.contrib.auth.models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('auth', '0006_require_contenttypes_0002'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='League',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(unique=True, max_length=32)),
+                ('admin', models.EmailField(max_length=254)),
+                ('fee', models.DecimalField(max_digits=5, decimal_places=2)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='User',
+            fields=[
+                ('user_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to=settings.AUTH_USER_MODEL)),
+                ('campaign', models.TextField()),
+                ('league', models.ForeignKey(to='frontend.League')),
+            ],
+            options={
+                'abstract': False,
+                'verbose_name': 'user',
+                'verbose_name_plural': 'users',
+            },
+            bases=('auth.user',),
+            managers=[
+                ('objects', django.contrib.auth.models.UserManager()),
+            ],
+        ),
+    ]
--- frontend/models.py
+++ frontend/models.py
@@ -1,12 +1,21 @@
 import datetime
 from django.utils import timezone
 from django.db import models
-from django.contrib.auth.models import User as DjangoUser
+from django.contrib.auth.models import User
 
 # Create your models here.
 class League(models.Model):
 	name = models.CharField(max_length=32, unique=True)
+	admin = models.ForeignKey(User)
+	fee = models.DecimalField(max_digits=5,decimal_places=2)
 
-class User(DjangoUser):
+	def __str__(self):
+		return self.name
+
+class Bet(models.Model):
 	league = models.ForeignKey(League)
-	campaign = models.TextField()
+	user = models.ForeignKey(User)
+	campaign = models.URLField()
+
+	def __str__(self):
+		return self.league.name + ":" + self.user.username
